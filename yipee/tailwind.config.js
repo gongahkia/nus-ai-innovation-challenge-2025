e47eb4f2ee2291fa9f311e9b2e732d9e61a1b260
gongahkia <angryapplegravy@gmail.com>
2025-02-28 14:45:56 +0800

    feat(app): minimal frontend

--- yipee/tailwind.config.js
+++ yipee/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}", "*.{js,ts,jsx,tsx,mdx}"],
+  content: ["app/**/*.{js,jsx,ts,tsx}", "components/**/*.{js,jsx,ts,tsx}", "*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     container: {
       center: true,
