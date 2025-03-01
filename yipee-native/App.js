47416ebf4c12a7498571578d57dcffe8b7c8b8b0
gongahkia <angryapplegravy@gmail.com>
2025-03-01 12:04:31 +0800

    fix(app): app navigatorrr fuck you!!!

--- yipee-native/App.js
+++ yipee-native/App.js
@@ -2,15 +2,12 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
 import { AppProvider } from './src/context/AppContext';
-import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <AppProvider>
       <SafeAreaProvider>
-        <NavigationContainer>
-          <AppNavigator />
-        </NavigationContainer>
+        <AppNavigator />
       </SafeAreaProvider>
     </AppProvider>
   );
